2776541275391546e625db037eff785308deddb8
SaminYT5 <saminiscool5@gmail.com>
2026-05-06 14:50:29 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const jsbutton = document.getElementById('jsbutton');
 
-jsbutton.onclick = () => {
-    alert('Fuck you🖕 this alert is coded in JS');
+if (jsbutton) {
+    jsbutton.onclick = () => {
+        alert('This alert is coded in JS the script is mainly for the multiple pages in one HTML file the script was originally just to test JS');
+    }
 }
+
+function showSection(sectionId) {
+    document.querySelectorAll('section').forEach(section => {
+        section.style.display = 'none';
+    });
+    
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        targetSection.style.display = 'block';
+    }
+}
+
+showSection('home');
+
+window.addEventListener('hashchange', () => {
+    const hash = window.location.hash.slice(1) || 'home';
+    showSection(hash);
+});
